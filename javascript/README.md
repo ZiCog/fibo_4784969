fc2796992ab764bc3114fff4dbdce620d6650a7b
zicog <zicog@example.com>
2018-12-23 23:17:41 +0200

    Added a somewhat faster JS fibo, fiboFaster()

--- javascript/README.md
+++ javascript/README.md
@@ -15,11 +15,24 @@ The fibo calculation takes a mere 12 seconds on an x86 PC. But producing the mil
     user    0m57.766s
     sys     0m0.172s
 
+UPDATE: 23 Dec 2018
+
+Now includes a fatser fibo inspired by Paeryn's Hasakell FiboFast version.  Now nearly twice as fast!
+
+    $ time node fibo.js
+    7143ms
+    ....
+    152076875379936330930391815964864885353407167474856539211500699706378405156269
+
+    real    0m50.357s
+    user    0m50.125s
+    sys     0m0.109s
+
 ## fibo_karatsuba.js
 
 By heater on the Raspberry Pi forum https://www.raspberrypi.org/forums/viewtopic.php?f=62&t=227343&start=500#p1402101
 
-Uses a hand made karatsuba big integer multiplication algorith rather than the BigInt type of recent JS standards. Mostly as just an exercise in creating a karatuba.
+Uses a hand made karatsuba big integer multiplication algorith rather than the BigInt type of recent JS standards. Mostly as just an exercise in creating a karatsuba.
 
 Runs on an x86 PC in some minutes..
 
--- javascript/fibo.js
+++ javascript/fibo.js
@@ -11,17 +11,32 @@ let memo = [BigInt(0), BigInt(1), BigInt(1)]
 
 function fibo (n) {
   if (memo[n]) {
-    let res = memo[n]
-    return res
+    return memo[n]
   }
   let k = (n / 2) | 0
+  let a = fibo(k);
+  let b = fibo(k + 1);
   if (isEven(n)) {
-    let fk = fibo(k);
-    let res = memo[n] = fk * ((fibo(k + 1) * 2n) - fk)
-    return res
+    return memo[n] = a * ((b * 2n) - a)
   }
-  let res = memo[n] = fibo(k) ** 2n + fibo(k + 1) ** 2n
-  return res
+  return memo[n] = a ** 2n + b ** 2n
+}
+
+function fiboFast (n) {
+  if (typeof memo[n] != 'undefined') {
+    return memo[n]
+  }
+
+  let k = (n / 2) | 0
+  let a = fiboFast(k)
+  let b = fiboFast(k - 1)
+  if (isEven(n)) {
+    return memo[n] = a * (2n * b + a)
+  }
+  if ((n % 4) === 1) {
+    return memo[n] = (2n * a + b) * (2n * a - b) + 2n
+  }
+  return memo[n] = (2n * a + b) * (2n * a - b) - 2n
 }
 
 let res
@@ -33,19 +48,6 @@ function timeIt (f, k) {
   console.log(dur + "ms")
 }
 
-timeIt(fibo, 4784969)
+//timeIt(fibo, 4784969)
+timeIt(fiboFast, 4784969)
 console.log(res.toString(10))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
