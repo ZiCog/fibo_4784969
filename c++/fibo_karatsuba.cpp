63acdb31f751b1bafacbd599c0311de93eb1270a
zicog <zicog@example.com>
2018-12-30 18:08:29 +0200

    Removed need for MUTABLE. Fixed up ejolson's fibo

--- c++/fibo_karatsuba.cpp
+++ c++/fibo_karatsuba.cpp
@@ -4,64 +4,45 @@
 
 #include "bint.h"
 
+//using fint = bint<>;
+
 const bint zero = "0";
 const bint one = "1";
 const bint two = "2";
 
-int isEven(int n) { return (n & 1) == 0; }
-
 std::unordered_map<uint64_t, bint> memo;
 
-#if MUTABLE
-bint fibo(int n) {
-    if (memo.find(n) != memo.end()) {
-        return memo[n];    
+int isEven(int n) { return (n & 1) == 0; }
+
+// This Fibonacci version derived from ejolson's doubling formula C example.
+bint a;
+bint b;
+static void fiboEjOlson(const int n) {
+    if (n == 0) {
+        a = zero;
+        b = one;
+        return;
     }
-    int k = (n / 2);
-    bint fk = fibo(k);
-    bint fk1 = fibo(k + 1);
+    fiboEjOlson(n / 2);
+    bint ta = a;
     if (isEven(n)) {
-        bint result = fk * (fk1 * two - fk); 
-        memo[n] = result;
-        return result;
-    }
-    bint result = (fk * fk) + (fk1 * fk1);
-    memo[n] = result;
-    return result; 
-}
-#else
-const bint fiboEjolson (int n)
-{
-    switch (n)
-    {
-        case 0:
-            return zero;
-        case 1:
-            return one;
-        case 2:
-            return one;
-        default:
-            int k = (n / 2);
-            const bint fk = fibo(k);
-            const bint fk1 = fibo(k + 1);
-            if (isEven(n))
-            {
-                return fk * (fk1 * two - fk);
-            }
-            return (fk * fk) + (fk1 * fk1);
+        a = ta * ((b + b) - ta);
+        b = (ta * ta) + (b * b);
+    } else {
+        a = (a * a) + (b * b);
+        b = b * ((ta + ta) + b);
     }
 }
-#endif
 
-// This version derived from Paeryn's Haskell example.
-const bint fiboFast (int n) {
+// This Fibonacci version derived from Paeryn's Haskell example.
+const bint fibo (int n) {
     if (memo.find(n) != memo.end()) {
         return memo[n];    
     }
 
     int k = (n / 2);
-    const bint a = fiboFast(k);
-    const bint b = fiboFast(k - 1);
+    const bint a = fibo(k);
+    const bint b = fibo(k - 1);
     if (isEven(n)) {
         return memo[n] = a * (two * b + a);
     }
@@ -76,13 +57,15 @@ bint timeIt(int n) {
     double elapsedTime;
     double startTime = clock() / CLOCKS_PER_SEC;
 
-#if MUTABLE
+    // Initialize the fibo's memo.
     memo.clear();
     memo[0] = zero;
     memo[1] = one;
     memo[2] = one;
-#endif
-    bint res = fiboFast(n);
+
+    bint res = fibo(n);
+//    fiboEjOlson(n - 1);
+//    bint res = b;
 
     endTime = (float)clock()/CLOCKS_PER_SEC;
     elapsedTime = endTime - startTime;
