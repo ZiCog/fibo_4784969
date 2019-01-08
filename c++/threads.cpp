75be9c597f470bb89d1e8522c774acc2a9c1a02e
zicog <zicog@example.com>
2019-01-08 20:21:51 +0200

    Fixed bugs in threads benchmark

--- c++/threads.cpp
+++ c++/threads.cpp
@@ -14,10 +14,10 @@
 bint res1;
 bint res2;
 
-bint simplefunc(int n)
+bint power(int n)
 {
-	bint x = 1;
-	bint ten = "10";
+	bint x = bint("1");
+	bint ten = bint("10");
 	for (int i = 0; i < n; i++) { 
 		x = x * ten;
 	}
@@ -30,25 +30,22 @@ bint simplefunc(int n)
 static void BM_mulThreaded(benchmark::State& state) {
 	int n = state.range(0);
 	for (auto _ : state) {
-		auto future1 = std::async(simplefunc, n);
-		auto future2 = std::async(simplefunc, n);
-		res1 = future1.get();
-		res2 = future2.get();
-//			std::cout << res << '\n';
+		auto future1 = std::async(power, n);
+		auto future2 = std::async(power, n);
+		benchmark::DoNotOptimize(res1 = future1.get());
+		benchmark::DoNotOptimize(res2 = future2.get());
 	}
 }
-BENCHMARK(BM_mulThreaded)->RangeMultiplier(2)->Range(1<<4, 1<<14)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_mulThreaded)->RangeMultiplier(2)->Range(1<<4, 1<<16)->Unit(benchmark::kMillisecond);
 
 // Define mul benchmark
 static void BM_mul(benchmark::State& state) {
 	int n = state.range(0);
 	for (auto _ : state) {
-		res1 = simplefunc(n);
-		res2 = simplefunc(n);
-//			std::cout << res << '\n';
+		benchmark::DoNotOptimize(res1 = power(n));
+		benchmark::DoNotOptimize(res2 = power(n));
 	}
 }
-BENCHMARK(BM_mul)->RangeMultiplier(2)->Range(1<<4, 1<<14)->Unit(benchmark::kMillisecond);
-
+BENCHMARK(BM_mul)->RangeMultiplier(2)->Range(1<<4, 1<<16)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
