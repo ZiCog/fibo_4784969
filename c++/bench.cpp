b7e32ab12b72e673e2b2a6040eeb935712ead761
zicog <zicog@example.com>
2019-01-10 19:42:24 +0200

    Only benchmark our fatsest fibo(4784969), added gnu plot script

--- c++/bench.cpp
+++ c++/bench.cpp
@@ -8,6 +8,8 @@
 #include "bint.h"
 #include "fibo.h"
 
+#define MAX_POW_2 22
+
 bool fiboInitialized = false;
 
 // Define fibo benchmark
@@ -22,8 +24,8 @@ static void BM_fibo(benchmark::State& state) {
 	}
 }
 
-BENCHMARK(BM_fibo)->Arg(4784969)->Unit(benchmark::kMillisecond);
-//BENCHMARK(BM_fibo)->RangeMultiplier(2)->Range(1<<4, 1<<27)->Unit(benchmark::kMillisecond);
+//BENCHMARK(BM_fibo)->Arg(4784969)->Unit(benchmark::kMillisecond);
+//BENCHMARK(BM_fibo)->RangeMultiplier(2)->Range(1<<4, 1<<MAX_POW_2)->Unit(benchmark::kMillisecond);
 
 bint a;
 bint b;
@@ -36,8 +38,8 @@ static void BM_fiboEjOlson(benchmark::State& state) {
 	}
 }
 
-BENCHMARK(BM_fiboEjOlson)->Arg(4784969)->Unit(benchmark::kMillisecond);
-//BENCHMARK(BM_fiboEjOlson)->RangeMultiplier(2)->Range(1<<4, 1<<27)->Unit(benchmark::kMillisecond);
+//BENCHMARK(BM_fiboEjOlson)->Arg(4784969)->Unit(benchmark::kMillisecond);
+//BENCHMARK(BM_fiboEjOlson)->RangeMultiplier(2)->Range(1<<4, 1<<MAX_POW_2)->Unit(benchmark::kMillisecond);
 
 // Define fibo benchmark
 static void BM_fiboEjOlsonNew(benchmark::State& state) {
@@ -48,8 +50,7 @@ static void BM_fiboEjOlsonNew(benchmark::State& state) {
 }
 
 BENCHMARK(BM_fiboEjOlsonNew)->Arg(4784969)->Unit(benchmark::kMillisecond);
-//BENCHMARK(BM_fiboEjOlson)->RangeMultiplier(2)->Range(1<<4, 1<<27)->Unit(benchmark::kMillisecond);
-
+//BENCHMARK(BM_fiboEjOlsonNew)->RangeMultiplier(2)->Range(1<<4, 1<<MAX_POW_2)->Unit(benchmark::kMillisecond);
 
 
 BENCHMARK_MAIN();
--- c++/benchmark_results.plot
+++ c++/benchmark_results.plot
@@ -8,5 +8,5 @@ set logscale xy
 set xlabel 'n'
 set ylabel 'Time (ms)'
 set title 'Fibo Comparison'
-plot  "data.txt" using 1:2 title 'Heater' with lines, "data.txt" using 1:3 title 'ejolson' with lines
+plot  "data.txt" using 1:2 title 'Heater' with lines, "data.txt" using 1:3 title 'ejolson' with lines, "data.txt" using 1:4 title 'ejolsonNew' with lines
 
