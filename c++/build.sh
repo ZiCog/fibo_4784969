7859bf5e9b1413a38d9292ed989457a91e9add8d
michael <michael@conveqs.fi>
2019-12-10 09:21:08 +0000

    OMP build now called fibo_karatsuba_omp

--- c++/build.sh
+++ c++/build.sh
@@ -2,7 +2,7 @@
 
 g++ -Wall -O3 -o fibo_karatsuba fibo_karatsuba.cpp fibo.cpp
 
-g++ -Wall -O3 -DUSE_OMP -fopenmp -o fibo_karatsuba fibo_karatsuba.cpp fibo.cpp
+g++ -Wall -O3 -DUSE_OMP -fopenmp -o fibo_karatsuba_omp fibo_karatsuba.cpp fibo.cpp
 
 
 
