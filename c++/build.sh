cab1679822bac9ef5b8271ed1e17833d46193135
Michaael Rychlik <michaael@Michaaels-MacBook-Pro.local>
2021-02-23 11:53:55 +0200

    Tweaked build file to use g++-10 on MacOS. Also to build with clang on MacOS which has no openmp

--- c++/build.sh
+++ c++/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-g++ -Wall -O3 -o fibo_karatsuba fibo_karatsuba.cpp fibo.cpp
 
-g++ -Wall -O3 -DUSE_OMP -fopenmp -o fibo_karatsuba_omp fibo_karatsuba.cpp fibo.cpp
+
+g++-10 -std=c++17 -Wall -O3 -o fibo_karatsuba fibo_karatsuba.cpp fibo.cpp 
+
+g++-10 -std=c++17 -Wall -O3 -DUSE_OMP -fopenmp -o fibo_karatsuba_omp fibo_karatsuba.cpp fibo.cpp
 
 
 
--- c++/fibo_karatsuba.cpp
+++ c++/fibo_karatsuba.cpp
@@ -2,7 +2,10 @@
 
 #include "bint.h"
 #include "fibo.h"
+
+#ifdef USE_OMP
 #include <omp.h>
+#endif
 
 int main(int argc, char *argv[]) {
     int n = 4784969;   // The first Fibonacci number with a million digits
